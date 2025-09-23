dd98bcff88ba8b053f09171a05d3b86e9bb8df4d
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-09-23 16:22:09 +0200

    add admin for project

--- apps/project/admin.py
+++ apps/project/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Project, Tag, Image
 
-# Register your models here.
+admin.site.register(Tag)
+
+
+class ImageInline(admin.TabularInline):
+    model = Image
+    extra = 3
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    inlines = [ImageInline]
+

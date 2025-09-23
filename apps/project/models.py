17a786a3a396be78ce76171370961052808a59f6
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-09-23 15:38:41 +0200

    add models

--- apps/project/models.py
+++ apps/project/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from portfolio.storage_backends import ProjectImagesStorage
+from utils.nameHandler import rename_file_to_upload
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Image(models.Model):
+    image = models.ImageField(
+        upload_to=rename_file_to_upload,
+        storage=ProjectImagesStorage(),
+        blank=True,
+        null=True,
+    )
+
+    def __str__(self):
+        return self.image.url if self.image else "No Image"
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    github_link = models.URLField(blank=True, null=True)
+    live_link = models.URLField(blank=True, null=True)
+    tags = models.ManyToManyField(Tag, related_name="projects")
+    placeholder_image = models.ForeignKey(
+        Image,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="placeholder_for",
+    )
+    images = models.ManyToManyField(Image, related_name="projects", blank=True)
+
+    def __str__(self):
+        return self.name

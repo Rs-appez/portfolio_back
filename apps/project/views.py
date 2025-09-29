05e48c164ae451c1146c81d6ce8bdffd8fdb073b
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-09-29 13:46:55 +0200

    add views

--- apps/project/views.py
+++ apps/project/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from .models import Project, Tag, Image
+from .serializers import ProjectSerializer, TagSerializer, ImageSerializer
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import DjangoModelPermissions
 
-# Create your views here.
+
+class ProjectViewSet(ModelViewSet):
+    permission_classes = [DjangoModelPermissions]
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+
+class TagViewSet(ModelViewSet):
+    permission_classes = [DjangoModelPermissions]
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+
+
+class ImageViewSet(ModelViewSet):
+    permission_classes = [DjangoModelPermissions]
+    queryset = Image.objects.all()
+    serializer_class = ImageSerializer

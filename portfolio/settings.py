95be00ad90df9843bb749a534fbd97568d03ba84
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-09-29 09:58:12 +0200

    add authtoken restframework

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -29,7 +29,12 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    
+    "rest_framework",
+    "rest_framework.authtoken",
+
     "storages",
+
     "apps.project",
 ]
 

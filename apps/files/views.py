dc930f669b45aa5ad5afa88f39cc6a9195ad8c1d
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-10-05 19:58:03 +0200

    add endpoint to get the latest resume

--- apps/files/views.py
+++ apps/files/views.py
@@ -1,6 +1,8 @@
 from .models import CV
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import DjangoModelPermissions
+from rest_framework.response import Response
+from rest_framework.decorators import action
 from .serializers import CVSerializer
 
 
@@ -8,3 +10,9 @@ class CVViewSet(ModelViewSet):
     permission_classes = [DjangoModelPermissions]
     queryset = CV.objects.all()
     serializer_class = CVSerializer
+
+    @action(detail=False, methods=["get"])
+    def latest(self, request):
+        latest_cv = CV.objects.latest("uploaded_at")
+        serializer = self.get_serializer(latest_cv)
+        return Response(serializer.data)

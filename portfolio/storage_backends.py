6515b89fed251ffc8dfaaaab27e42cf23d4240c2
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-10-05 11:37:17 +0200

    add storage for cv

--- portfolio/storage_backends.py
+++ portfolio/storage_backends.py
@@ -4,3 +4,8 @@ from storages.backends.s3boto3 import S3Boto3Storage
 class ProjectImagesStorage(S3Boto3Storage):
     location = "project_images"
     file_overwrite = False
+
+
+class CvStorage(S3Boto3Storage):
+    location = "cv"
+    file_overwrite = True
